073bc5c891229e806134d2712b52196b2356bc7b
maxporyvay <sevmaxclever@gmail.com>
2023-11-26 14:21:53 +0300

    If-else-endif is now replaceable

--- render_template.py
+++ render_template.py
@@ -1,19 +1,50 @@
 import re
 
 
-def replace_words(text, replacements):
+def replace_ifs(text, replacements):
+
+    def re_match(match):
+        condition = bool(eval(match.group(1).strip(), replacements))
+        code_if = match.group(2).strip()
+        code_else = match.group(3).strip()
+        print(condition, code_if, code_else)
+
+        code_to_replace = code_if if condition else code_else
+
+        return code_to_replace
+
+    pattern = r"{% if(.+?)%}(.+?){% else %}(.+?){% endif %}"
+    result = re.sub(pattern, re_match, text, flags=re.DOTALL)
+    return result
+
+
+def replace_fors(text, replacements):
+    return text
+
+
+def replace_vars(text, replacements):
+
     def re_match(match):
         return str(eval(match.group(1), replacements))
+
     pattern = r"{{ (.+?) }}"
     result = re.sub(pattern, re_match, text)
     return result
+
+
+def make_replaces(text, replacements):
+    
+    text = replace_ifs(text, replacements)
+    text = replace_fors(text, replacements)
+    text = replace_vars(text, replacements)
+    return text
 
 
 def render_template(html_filename, context):
     with open(html_filename) as html:
         html_text = html.read()
 
-    return replace_words(html_text, context)
+    return make_replaces(html_text, context)
 
 
 def main():
@@ -28,7 +59,7 @@ def main():
 
     title = 'TITLE'
     message = 'MESSAGE'
-    usr = User(42)
+    usr = User(32)
 
     cntxt = locals()
     print(render_template('template_plain.html', cntxt))
