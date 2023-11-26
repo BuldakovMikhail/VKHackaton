5e9d03130035cb3a8f53de4bd5319138c19552dc
Buldakov Mikhail <mishabul2003@gmail.com>
2023-11-26 13:55:01 +0300

    without cookies

--- response/response.py
+++ response/response.py
@@ -1,14 +1,7 @@
-# class CookieObj:
-#     def __init__(self, value, expire_time):
-#         self.value = value
-#         self.expire_time = expire_time
-
-
 class Response:
     def __init__(self) -> None:
         self.__body = ""  # Строка ответ хендлера
         self.__headers = []  # Заголовки созданные хендлером
-        # self.__cookies = {}
 
     def __str__(self) -> str:
         response_str = "\r\n".join(self.__headers) + "\r\n\r\n" + self.__body
@@ -33,22 +26,8 @@ class Response:
     def append_header(self, header: str) -> None:
         self.__headers.append(header)
 
-    # def append_cookie(self, key, cookie_data):
-    #     self.__cookies[key] = cookie_data
-
-    # def set_cookie()
-
-    # @property
-    # def cookies(self):
-    #     return self.__cookies
-
-    # @cookies.setter
-    # def cookies(self, cookies):
-    #     self.__cookies = cookies
-
 
 if __name__ == "__main__":
-    # cookies = CookieData(1, "time of expire")
     resp = Response()
     resp.body = "shreck"
     resp.headers = ["boloto Shrecka", "Pivo"]
