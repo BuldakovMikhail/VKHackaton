023394c9d9bdff1e19788f4a9a64a697cb5f142b
DeadPizza <alesha_r_2003@mail.ru>
2023-11-26 17:13:58 +0300

    final?

--- main.py
+++ main.py
@@ -5,12 +5,26 @@ from nn_framework.Response import Response
 
 app = WebServer()
 
+@app.add_handler('/')
+def index_handler(session: ReqParser):
+    resp = Response()
+    resp.body = render_template('./main_page.html', {})
+    return resp
+
 @app.add_handler('/refresh')
 def refresh_handler(session: ReqParser):
     n_amount = session.get_cookies().get('n_amount', 0)
     resp = Response()
     resp.body = render_template('./count_refresh.html', locals())
     resp.set_cookie('n_amount', int(n_amount) + 1, 60 * 60 * 24)
+    return resp
+
+@app.add_handler('/binding/<test>')
+def binding_example(session: ReqParser):
+    test = session.url.replace('/binding/', '')
+    n_amount = session.get_cookies().get('n_amount', 0)
+    resp = Response()
+    resp.body = render_template('./binding_test.html', locals())
     return resp
 
 app.run('0.0.0.0', 8080)
